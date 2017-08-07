72e0ce7bc61b1b04c6c488fd915408b335250cc9
Kohki Makimoto <kohki.makimoto@gmail.com>
2017-08-08 07:57:29 +0900

    fix misstake

--- adminer-cli.rb
+++ adminer-cli.rb
@@ -1,7 +1,7 @@
 class AdminerCli < Formula
   desc "Command line interface to run adminer on the PHP built-in server."
   homepage "https://github.com/kohkimakimoto/adminer-cli"
-  version '0.4.0'
+  version '0.5.0'
   url "https://github.com/kohkimakimoto/adminer-cli/archive/v0.5.0.tar.gz"
   sha256 "9ba6652073005f3d8f54c0b069b6b9786cae9975f5e62f06e62e00c4c8d12077"
 

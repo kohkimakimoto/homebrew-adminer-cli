e1e34db5046c5dbd0f65c865b8bb1b9972d12cdc
Kohki Makimoto <kohki.makimoto@gmail.com>
2017-08-08 10:47:21 +0900

    update

--- adminer-cli.rb
+++ adminer-cli.rb
@@ -1,13 +1,13 @@
 class AdminerCli < Formula
   desc "Command line interface to run adminer on the PHP built-in server."
   homepage "https://github.com/kohkimakimoto/adminer-cli"
-  version '0.7.0'
-  url "https://github.com/kohkimakimoto/adminer-cli/archive/v0.7.0.tar.gz"
-  sha256 "704e79b7645db9a365ba88fd7a0267f044dd2cdcfdb00ea424a8d02d90cdb4d0"
+  version '0.9.0'
+  url "https://github.com/kohkimakimoto/adminer-cli/archive/v0.9.0.tar.gz"
+  sha256 "f8033a5b28a12091eed7efabca32b505ff18474ddafc306dd4616279790ab13b"
 
   def install
     prefix.install Dir["*"]
-    bin.install ["adminer"]
+    bin.install_symlink prefix/"adminer"
   end
 
   test do
